e6596a6e2ea0ebc922bb5f571160fb383b1f5ba6
Casey Carroll <caseydean8@gmail.com>
2019-07-01 09:25:21 -0400

    Cleaned up and added comments.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-// Initialize firebase
+// Initialize firebase.
 var config = {
     apiKey: 'AIzaSyAgc85qULigTIbF658xdHmrHrlIt33YVCc',
     authDomain: 'fir-jump-b7b14.firebaseapp.com',
@@ -12,17 +12,17 @@ var config = {
 firebase.initializeApp(config);
 var database = firebase.database();
 
-// Collect user input to send to firebase
+// Collect user input to send to firebase.
 $(".button").on("click", function(event) {
     event.preventDefault();
 
-    // collect user form inputs.
+    // Collect user form inputs.
     var trainName = $("#train-name").val().trim();
     var destination = $("#destination").val().trim();
     var firstTrain = $("#first-train").val().trim();
     var frequency = $("#frequency").val().trim();
     
-    // Create a temporary object to hold train data
+    // Create a temporary object to hold train data.
     var newTrain = {
         trainName: trainName,
         destination: destination,
@@ -38,22 +38,29 @@ $(".button").on("click", function(event) {
     database.ref().push(newTrain);
 });
 
+// Retrieve data from our database with "child_added" event.
 database.ref().on("child_added", function(childSnapshot) {
     var trainNameDisplay = childSnapshot.val().trainName;
     var destinationDisplay = childSnapshot.val().destination;
     var frequencyDisplay = childSnapshot.val().frequency;
     var firstTrainTime = childSnapshot.val().firstTrain;
     
+    // Subtract one year from first train time.
     var firstTrainConverted = moment(firstTrainTime, "HH:mm").subtract(1, "years");
 
+    // Determine difference between current time and first train time in minutes.
     var timeDiff = moment().diff(moment(firstTrainConverted), "minutes");
     
+    // Divide the difference by the frequency of train runnings.
     var timeDiffRemainder = timeDiff % frequencyDisplay;
-
+    
+    // Subtract the remainder from the frequency.
     var minutesTilTrain = frequencyDisplay - timeDiffRemainder;
 
+    // Add minutes until next train arrival to current time to display next train arrival time.
     var nextArrival = moment().add(minutesTilTrain, "minutes").format("HH:mm");
 
+    // Create a new table row with data displayed in correct table data element.
     var newRow = $("<tr>").append(
         $("<td>").text(trainNameDisplay),
         $("<td>").text(destinationDisplay),
@@ -61,5 +68,6 @@ database.ref().on("child_added", function(childSnapshot) {
         $("<td>").text(nextArrival),
         $("<td>").text(minutesTilTrain),
         )
+        // Add new table row to table body.
         $("tbody").append(newRow);
 });
